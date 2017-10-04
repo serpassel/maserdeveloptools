429f3c024a4c5c13411af7a5ae2b070c709549e9
sergio <spseligrat@gmail.com>
2017-10-04 14:53:01 +0200

    jbasictools jcenter 0.0.1 sqltools jcenter 0.0.1

---
+++ app/src/main/java/es/marser/maserdeveloptools/CompileGrand.java
@@ -0,0 +1,16 @@
+package es.marser.maserdeveloptools;
+
+
+import es.marser.TextTools;
+import es.marser.sqltools.SQLStrings;
+
+/**
+ * @author sergio
+ *         Created by sergio on 4/10/17.
+ */
+
+@SuppressWarnings("ALL")
+public class CompileGrand {
+String s = TextTools.nc("");
+    String s1 = SQLStrings.createFilter("filter");
+}
