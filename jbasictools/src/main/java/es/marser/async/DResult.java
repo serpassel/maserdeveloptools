ff72b805abcdfdb8c4ebfa68c1e452b97096beb8
sergio <spseligrat@gmail.com>
2017-11-08 19:47:13 +0100

    DialogInputBox

---
+++ jbasictools/src/main/java/es/marser/async/DResult.java
@@ -0,0 +1,19 @@
+package es.marser.async;
+
+/**
+ * @author sergio
+ *         Created by sergio on 26/10/17.
+ *         Clase para entrega de resultados en procesos asíncronos
+ *         <p>
+ *         [EN]  Class for delivery of results in asynchronous processes
+ */
+
+public interface DResult<T, X> {
+    /**
+     * Resultado de la finalización de una tarea
+     * <p>
+     *     [EN]  Result of completing a task
+     * @param result Objeto genérico procesado en la tarea [EN]  Processed generic object in the task
+     */
+    void onResult(int result, T value1, X value2);
+}
